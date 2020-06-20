8c1702c48ef5428ac7bf1d223098e62ad1c01456
robpetrin <robpetrin@gmail.com>
2020-06-19 23:28:24 -0400

    Updating version number

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: A tool for embedding CodePens without the need to deal with any code! (Made possible thanks to "create-guten-block" by mrahmadawais and maedahbatool)
  * Author: robpetrin
  * Author URI: https://petr.in
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
